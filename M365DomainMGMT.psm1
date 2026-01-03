72a13e76744a31998f9cade354dc8221b2b0cb6c
Tim McMichael <timmcmic@microsoft.com>
2026-01-03 12:22:13 -0500

    UPdating

---
+++ M365DomainMGMT.psm1
@@ -0,0 +1,393 @@
+#############################################################################################
+# DISCLAIMER:																				#
+#																							#
+# THE SAMPLE SCRIPTS ARE NOT SUPPORTED UNDER ANY MICROSOFT STANDARD SUPPORT					#
+# PROGRAM OR SERVICE. THE SAMPLE SCRIPTS ARE PROVIDED AS IS WITHOUT WARRANTY				#
+# OF ANY KIND. MICROSOFT FURTHER DISCLAIMS ALL IMPLIED WARRANTIES INCLUDING, WITHOUT		#
+# LIMITATION, ANY IMPLIED WARRANTIES OF MERCHANTABILITY OR OF FITNESS FOR A PARTICULAR		#
+# PURPOSE. THE ENTIRE RISK ARISING OUT OF THE USE OR PERFORMANCE OF THE SAMPLE SCRIPTS		#
+# AND DOCUMENTATION REMAINS WITH YOU. IN NO EVENT SHALL MICROSOFT, ITS AUTHORS, OR			#
+# ANYONE ELSE INVOLVED IN THE CREATION, PRODUCTION, OR DELIVERY OF THE SCRIPTS BE LIABLE	#
+# FOR ANY DAMAGES WHATSOEVER (INCLUDING, WITHOUT LIMITATION, DAMAGES FOR LOSS OF BUSINESS	#
+# PROFITS, BUSINESS INTERRUPTION, LOSS OF BUSINESS INFORMATION, OR OTHER PECUNIARY LOSS)	#
+# ARISING OUT OF THE USE OF OR INABILITY TO USE THE SAMPLE SCRIPTS OR DOCUMENTATION,		#
+# EVEN IF MICROSOFT HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES						#
+#############################################################################################
+
+
+Function Start-M365DomainManagement
+{
+    
+    <#
+    .SYNOPSIS
+
+    This module helps automate domain management in Microsoft 365.
+
+    .DESCRIPTION
+
+    This function triggers all sub functions..
+
+    .PARAMETER LogFolderPath
+
+    *REQUIRED*
+    This provides the logging directory for the application.
+
+    .PARAMETER GLOBALCATALOGSERVER
+
+    *REQUIRED*
+    This attribute specifies the global catalog server that will be utilized to process Active Directory commands.
+
+    .PARAMETER ACIVEDIRECTORYCREDENTIAL
+
+    *REQUIRED*
+    This attribute specifies the credentials for Active Directory connections.
+    Domain admin credentials are required if the group does not have resorces outside of the domain where the group resides.
+    Enterprise admin credentials are required if the group has resources across multiple domains in the forest.
+
+    .PARAMETER ACTIVEDIRECTORYAUTHENTICATIONMETHOD
+
+    Allows the administrator to specify kerberos or basic authentication for connections to Active Directory.
+
+    .PARAMETER AADCONNECTSERVER
+
+    *OPTIONAL*
+    This parameter specifies the FQDN of the Azure Active Directory Connect Server.
+    When specified the server is utilized to trigger delta syncs to provide timely migrations.
+    If not specified the script will wait for standard sync cycles to run.
+
+    .PARAMETER AADCONNECTCREDENTIAL
+
+    *OPTIONAL*
+    *MANDATORY with AADConnectServer specified*
+    This parameter specifies the credentials used to connect to the AADConnect server.
+    The account specified must be a member of the local administrators sync group of the AADConnect Server
+
+    .PARAMETER AADCONNECTAUTHENTICATIONMETHOD
+
+    Allows the administrator to specify kerberos or basic authentication for connections to the AADConnect server.
+
+    .PARAMETER EXCHANGESERVER
+
+    *OPTIONAL*
+    *REQUIRED with enableHybridMailFlow:TRUE*
+    This parameter specifies that local Exchange on premises installation utilized for hybrid mail flow enablement.
+    Exchange server is no required for migrations unlss enable hyrbid mail flow is required.
+
+    .PARAMETER EXCHANGECREDENTIAL
+
+    *OPTIONAL*
+    *REQUIRED with ExchangeServer specified*
+    This is the credential utilized to connect to the Exchange server remote powershell instance.
+    Exchange Organization Adminitrator rights are recommended.
+
+    .PARAMETER EXCHANGEAUTHENTICATIONMETHOD
+
+    *OPTIONAL*
+    *DEFAULT:  BASIC*
+    This specifies the authentication method for the Exchage on-premsies remote powershell session.
+
+    .PARAMETER EXCHANGEONLINECREDENTIAL
+
+    *REQUIRED if ExchangeOnlineCertificateThumbprint not specified*
+    *NOT ALLOWED if ExchangeCertificateThubprint is specified*
+    The credential utilized to connect to Exchange Online.
+    This account cannot have interactive logon requirements such as multi-factored authentication.
+    Exchange Organization Administrator rights recommened.
+
+    .PARAMETER EXCHANGEONLINECERTIFICATETHUMBPRINT
+
+    *REQUIRED if ExchangeOnlineCredential is not specified*
+    *NOT ALLOWED if ExchangeCredential is specified*
+    This is the thumbprint of the certificate utilized to authenticate to the Azure application created for Exchange Certificate Authentication
+
+    .PARAMETER EXCHANGEONLINEORGANIZATIONNAME
+
+    *REQUIRED only with ExchangeCertificateThumbpint*
+    This specifies the Exchange Online oragnization name in domain.onmicroosft.com format.
+
+    .PARAMETER EXCHANGEONLINEENVIRONMENTNAME
+
+    *OPTIONAL*
+    *DEFAULT:  O365DEFAULT
+    This specifies the Exchange Online environment to connect to if a non-commercial forest is utilized.
+
+    .PARAMETER EXCHANGEONLINEAPPID
+
+    *REQUIRED with ExchangeCertificateThumbprint*
+    This specifies the application ID of the Azure application for Exchange certificate authentication.
+
+    .PARAMETER AZUREADCREDENTIAL
+
+    *REQUIRED if AzureCertificateThumbprint is not specified*
+    This is the credential utilized to connect to Azure Active Directory.
+    Global administrator is the tested permissions set / minimum permissions to execute get-azureADGroup
+
+    .PARAMETER AZUREENVRONMENTNAME
+
+    *OPTIONAL*
+    *DEFAULT:  AzureCloud*
+    This is the Azure tenant type to connect to if a non-commercial tenant is used.
+
+    .PARAMETER AZURETENANTID
+
+    *REQUIRED if AzureCertificateThumbprint is specified*
+    This is the Azure tenant ID / GUID utilized for Azure certificate authentication.
+
+    .PARAMETER AZURECERTIFICATETHUMBPRINT
+
+    *REQUIRED if AzureADCredential is not specified*
+    This is the certificate thumbprint associated with the Azure app id for Azure certificate authentication
+
+    .PARAMETER AZUREAPPLICATIONID
+
+    *REQUIRED if AzureCertificateThumbprint is specified*
+    This is the application ID assocaited with the Azure application created for certificate authentication.
+
+    .PARAMETER LOGFOLDERPATH
+
+    *REQUIRED*
+    This is the logging directory for storing the migration log and all backup XML files.
+    If running multiple SINGLE instance migrations use different logging directories.
+
+    .PARAMETER doNoSyncOU
+
+    *REQUIRED*
+    This is the organizational unit configured in Azure AD Connect to not sync.
+    This is utilize for temporary group storage to process the deletion of the group from Office 365.
+
+    .PARAMETER RETAINORIGINALGROUP
+
+    *OPTIONAL*
+    By default the original group is retained, mail disabled, and renamed with an !.
+    If the group should be deleted post migration set this value to TRUE.
+
+    .PARAMETER ENBABLEHYBRIDMAILFLOW
+
+    *OPTIONAL*
+    *REQUIRES use of ExchangeServer and ExchangeCredential*
+    This option enables mail flow objects in the on-premises Active Directory post migration.
+    This supports relay scenarios through the onpremises Exchange organization.
+
+    .PARAMETER GROUPTYPEOVERRIDE
+
+    *OPTIONAL*
+    This allows the administrator to override the group creation type in Office 365.
+    For example, an on premises security group may be migrated to Office 365 as a distribution only list.
+    If any security dependencies are discovered during the migration this option is always overridden to preserve security and the settings.
+
+    .PARAMETER TRIGGERUPGRADETOOFFICE365GROUP
+
+    *OPTIONAL*
+    *Parameter retained for backwards compatibility but now disabled.*
+
+    .PARAMETER OVERRIDECENTRALIZEDMAILTRANSPORTENABLED
+
+    *OPTIONAL*
+    If centralized transport enabled is detected during migration this switch is required.
+    This is an administrator acknowledgement that emails may flow externally in certain mail flow scenarios for migrated groups.
+
+    .PARAMETER ALLOWNONSYNCEDGROUP
+
+    *OPTIONAL*
+    Allows for on-premises group creation in Office 365 from forests that are not directory syncrhonized for some reason.
+
+    .PARAMETER USECOLLECTEDFULLMAILBOXACCESSONPREM
+
+    *OPTIONAL*
+    *Requires us of start-collectOnPremFullMailboxAccess*
+    This switch will import pre-collected full mailbox access data for the on premises organization and detect permissions for migrated DLs.
+
+    .PARAMETER USECOLLECTEDFULLMAILBOXACCESSOFFICE365
+
+    *OPTIONAL*
+    *Requires use of start-collectOffice365FullMailboxAccess
+    THis switch will import pre-collected full mailbox access data from the Office 365 organiation and detect permissions for migrated DLs.
+
+    .PARAMETER USERCOLLECTEDSENDASONPREM
+
+    *OPTIONAL*
+    *Requires use of start-collectOnPremSendAs*
+    This switch will import pre-collected send as data from the on premsies Exchange organization and detect dependencies on the migrated DLs.
+
+    .PARAMETER USECOLLECTEDFOLDERPERMISSIONSONPREM
+
+    *OPTIONAL*
+    *Requires use of start-collectOnPremMailboxFolderPermissions*
+    This switch will import pre-collected mailbox folder permissions for any default or user created folders within mailboxes.
+    The data is searched to discover any dependencies on the migrated DL.
+
+    .PARAMETER USECOLLECTEDFOLDERPERMISSIONSOFFICE365
+
+    *OPTIONAL*
+    *Requires use of start-collectOffice365MailboxFolderPermissions*
+    This switch will import pre-collected mailbox folder permissions for any default or user created folders within mailboxes.
+    The data is searched to discover any dependencies on the migrated DL.
+
+    .PARAMETER THREADNUMBERASSIGNED
+
+    *RESERVED*
+
+    .PARAMETER TOTALTHREADCOUNT
+
+    *RESERVED*
+
+    .PARAMETER ISMULTIMACHINE
+
+    *RESERVED*
+
+    .PARAMETER REMOTEDRIVELETTER
+
+    *RESERVED*
+
+    .PARAMETER ALLOWTELEMETRYCOLLECTION
+
+    Allows administrators to opt out of telemetry collection for DL migrations.  No identifiable information is collected in telemetry.
+
+    .PARAMETER ALLOWDETAILEDTELEMETRYCOLLECTIOn
+
+    Allows administrators to opt out of detailed telemetry collection.  Detailed telemetry collection includes information such as attribute member counts and time to process stages of the migration.
+
+    .PARAMETER ISHEALTHCHECK
+
+    Specifies if the function call is performing a distribution list health check.
+
+    .OUTPUTS
+
+    Logs all activities and backs up all original data to the log folder directory.
+    Moves the distribution group from on premieses source of authority to office 365 source of authority.
+
+    .NOTES
+
+    The following blog posts maintain documentation regarding this module.
+
+    https://timmcmic.wordpress.com.  
+
+    Refer to the first pinned blog post that is the table of contents.
+
+    
+    .EXAMPLE
+
+    Start-DistributionListMigration -groupSMTPAddress $groupSMTPAddress -globalCatalogServer server.domain.com -activeDirectoryCredential $cred -logfolderpath c:\temp -dnNoSyncOU "OU" -exchangeOnlineCredential $cred -azureADCredential $cred
+
+    .EXAMPLE
+
+    Start-DistributionListMigration -groupSMTPAddress $groupSMTPAddress -globalCatalogServer server.domain.com -activeDirectoryCredential $cred -logfolderpath c:\temp -dnNoSyncOU "OU" -exchangeOnlineCredential $cred -azureADCredential $cred -enableHybridMailFlow:$TRUE -triggerUpgradeToOffice365Group:$TRUE
+
+    .EXAMPLE
+
+    Start-DistributionListMigration -groupSMTPAddress $groupSMTPAddress -globalCatalogServer server.domain.com -activeDirectoryCredential $cred -logfolderpath c:\temp -dnNoSyncOU "OU" -exchangeOnlineCredential $cred -azureADCredential $cred -enableHybridMailFlow:$TRUE -triggerUpgradeToOffice365Group:$TRUE -useCollectedOnPremMailboxFolderPermissions:$TRUE -useCollectedOffice365MailboxFolderPermissions:$TRUE -useCollectedOnPremSendAs:$TRUE -useCollectedOnPremFullMailboxAccess:$TRUE -useCollectedOffice365FullMailboxAccess:$TRUE
+
+    #>
+
+    [cmdletbinding()]
+
+    Param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]$groupSMTPAddress,
+        #Local Active Director Domain Controller Parameters
+        [Parameter(Mandatory = $true)]
+        [string]$globalCatalogServer,
+        [Parameter(Mandatory = $true)]
+        [pscredential]$activeDirectoryCredential,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Basic","Negotiate")]
+        $activeDirectoryAuthenticationMethod="Negotiate",
+        #Azure Active Directory Connect Parameters
+        [Parameter(Mandatory = $false)]
+        [string]$aadConnectServer=$NULL,
+        [Parameter(Mandatory = $false)]
+        [pscredential]$aadConnectCredential=$NULL,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Basic","Kerberos")]
+        $aadConnectAuthenticationMethod="Kerberos",
+        #Exchange On-Premises Parameters
+        [Parameter(Mandatory = $false)]
+        [string]$exchangeServer=$NULL,
+        [Parameter(Mandatory = $false)]
+        [pscredential]$exchangeCredential=$NULL,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Basic","Kerberos")]
+        [string]$exchangeAuthenticationMethod="Kerberos",
+        #Exchange Online Parameters
+        [Parameter(Mandatory = $false)]
+        [pscredential]$exchangeOnlineCredential=$NULL,
+        [Parameter(Mandatory = $false)]
+        [string]$exchangeOnlineCertificateThumbPrint="",
+        [Parameter(Mandatory = $false)]
+        [string]$exchangeOnlineOrganizationName="",
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("O365Default","O365GermanyCloud","O365China","O365USGovGCCHigh","O365USGovDoD")]
+        [string]$exchangeOnlineEnvironmentName="O365Default",
+        [Parameter(Mandatory = $false)]
+        [string]$exchangeOnlineAppID="",
+        #Define Microsoft Graph Parameters
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("China","Global","USGov","USGovDod")]
+        [string]$msGraphEnvironmentName="Global",
+        [Parameter(Mandatory=$true)]
+        [string]$msGraphTenantID="",
+        [Parameter(Mandatory=$false)]
+        [string]$msGraphCertificateThumbprint="",
+        [Parameter(Mandatory=$false)]
+        [string]$msGraphApplicationID="",
+        [Parameter(Mandatory=$false)]
+        [boolean]$removeGroupViaGraph = $false,
+        #Define other mandatory parameters
+        [Parameter(Mandatory = $true)]
+        [string]$logFolderPath,
+        #Defining optional parameters for retention and upgrade
+        [Parameter(Mandatory = $false)]
+        [string]$dnNoSyncOU = "NotSet",
+        [Parameter(Mandatory = $false)]
+        [boolean]$retainOriginalGroup = $TRUE,
+        [Parameter(Mandatory = $false)]
+        [boolean]$enableHybridMailflow = $FALSE,
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("Security","Distribution","None")]
+        [string]$groupTypeOverride="None",
+        [Parameter(Mandatory = $false)]
+        [boolean]$triggerUpgradeToOffice365Group=$FALSE,
+        [Parameter(Mandatory=$false)]
+        [boolean]$overrideCentralizedMailTransportEnabled=$FALSE,
+        [Parameter(Mandatory=$false)]
+        [boolean]$allowNonSyncedGroup=$FALSE,
+        [Parameter(Mandatory=$false)]
+        [string]$customRoutingDomain="",
+        [Parameter(Mandatory=$false)]
+        $skipNestedGroupCheck=$false,
+        #Definte parameters for pre-collected permissions
+        [Parameter(Mandatory = $false)]
+        [boolean]$useCollectedFullMailboxAccessOnPrem=$FALSE,
+        [Parameter(Mandatory = $false)]
+        [boolean]$useCollectedFullMailboxAccessOffice365=$FALSE,
+        [Parameter(Mandatory = $false)]
+        [boolean]$useCollectedSendAsOnPrem=$FALSE,
+        [Parameter(Mandatory = $false)]
+        [boolean]$useCollectedFolderPermissionsOnPrem=$FALSE,
+        [Parameter(Mandatory = $false)]
+        [boolean]$useCollectedFolderPermissionsOffice365=$FALSE,
+        [Parameter(Mandatory = $false)]
+        [boolean]$useCollectedSendAsOffice365=$FALSE,
+        #Define paramters for naming conventions.
+        [Parameter(Mandatory = $false)]
+        [string]$dlNamePrefix="",
+        [Parameter(Mandatory = $false)]
+        [string]$dlNameSuffix="",
+        #Define parameters for multi-threaded operations
+        [Parameter(Mandatory = $false)]
+        [int]$threadNumberAssigned=0,
+        [Parameter(Mandatory = $false)]
+        [int]$totalThreadCount=0,
+        [Parameter(Mandatory = $FALSE)]
+        [boolean]$isMultiMachine=$FALSE,
+        [Parameter(Mandatory = $FALSE)]
+        [string]$remoteDriveLetter=$NULL,
+        [Parameter(Mandatory =$FALSE)]
+        [boolean]$allowTelemetryCollection=$TRUE,
+        [Parameter(Mandatory =$FALSE)]
+        [boolean]$allowDetailedTelemetryCollection=$TRUE,
+        [Parameter(Mandatory =$FALSE)]
+        [boolean]$isHealthCheck=$FALSE
+    )
+
