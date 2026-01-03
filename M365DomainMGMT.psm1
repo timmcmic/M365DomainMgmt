9fba570891e452842baa411dfb5c305e1e64a2de
Tim McMichael <timmcmic@microsoft.com>
2026-01-03 12:49:04 -0500

    Updating

--- M365DomainMGMT.psm1
+++ M365DomainMGMT.psm1
@@ -32,224 +32,25 @@ Function Start-M365DomainManagement
     *REQUIRED*
     This provides the logging directory for the application.
 
-    .PARAMETER GLOBALCATALOGSERVER
+    .PARAMETER msGraphTenantID
 
-    *REQUIRED*
-    This attribute specifies the global catalog server that will be utilized to process Active Directory commands.
+    This is the entra tenant ID where domain management should occur.
 
-    .PARAMETER ACIVEDIRECTORYCREDENTIAL
+    .PARAMETER msGraphEnvironmentName
 
-    *REQUIRED*
-    This attribute specifies the credentials for Active Directory connections.
-    Domain admin credentials are required if the group does not have resorces outside of the domain where the group resides.
-    Enterprise admin credentials are required if the group has resources across multiple domains in the forest.
+    This is the graph endpoint that will be utilized to manage the domain.
 
-    .PARAMETER ACTIVEDIRECTORYAUTHENTICATIONMETHOD
+    .PARAMETER msGraphCertificateThumbprint
 
-    Allows the administrator to specify kerberos or basic authentication for connections to Active Directory.
+    This is the certificate thumbprint installed locally and associated with graph certificate authentication.
 
-    .PARAMETER AADCONNECTSERVER
+    .PARAMETER mmsGraphAppicationID
 
-    *OPTIONAL*
-    This parameter specifies the FQDN of the Azure Active Directory Connect Server.
-    When specified the server is utilized to trigger delta syncs to provide timely migrations.
-    If not specified the script will wait for standard sync cycles to run.
+    This is the application GUID of the graph application created in entra.
 
-    .PARAMETER AADCONNECTCREDENTIAL
+    .PARAMETER msGraphClientSecret
 
-    *OPTIONAL*
-    *MANDATORY with AADConnectServer specified*
-    This parameter specifies the credentials used to connect to the AADConnect server.
-    The account specified must be a member of the local administrators sync group of the AADConnect Server
-
-    .PARAMETER AADCONNECTAUTHENTICATIONMETHOD
-
-    Allows the administrator to specify kerberos or basic authentication for connections to the AADConnect server.
-
-    .PARAMETER EXCHANGESERVER
-
-    *OPTIONAL*
-    *REQUIRED with enableHybridMailFlow:TRUE*
-    This parameter specifies that local Exchange on premises installation utilized for hybrid mail flow enablement.
-    Exchange server is no required for migrations unlss enable hyrbid mail flow is required.
-
-    .PARAMETER EXCHANGECREDENTIAL
-
-    *OPTIONAL*
-    *REQUIRED with ExchangeServer specified*
-    This is the credential utilized to connect to the Exchange server remote powershell instance.
-    Exchange Organization Adminitrator rights are recommended.
-
-    .PARAMETER EXCHANGEAUTHENTICATIONMETHOD
-
-    *OPTIONAL*
-    *DEFAULT:  BASIC*
-    This specifies the authentication method for the Exchage on-premsies remote powershell session.
-
-    .PARAMETER EXCHANGEONLINECREDENTIAL
-
-    *REQUIRED if ExchangeOnlineCertificateThumbprint not specified*
-    *NOT ALLOWED if ExchangeCertificateThubprint is specified*
-    The credential utilized to connect to Exchange Online.
-    This account cannot have interactive logon requirements such as multi-factored authentication.
-    Exchange Organization Administrator rights recommened.
-
-    .PARAMETER EXCHANGEONLINECERTIFICATETHUMBPRINT
-
-    *REQUIRED if ExchangeOnlineCredential is not specified*
-    *NOT ALLOWED if ExchangeCredential is specified*
-    This is the thumbprint of the certificate utilized to authenticate to the Azure application created for Exchange Certificate Authentication
-
-    .PARAMETER EXCHANGEONLINEORGANIZATIONNAME
-
-    *REQUIRED only with ExchangeCertificateThumbpint*
-    This specifies the Exchange Online oragnization name in domain.onmicroosft.com format.
-
-    .PARAMETER EXCHANGEONLINEENVIRONMENTNAME
-
-    *OPTIONAL*
-    *DEFAULT:  O365DEFAULT
-    This specifies the Exchange Online environment to connect to if a non-commercial forest is utilized.
-
-    .PARAMETER EXCHANGEONLINEAPPID
-
-    *REQUIRED with ExchangeCertificateThumbprint*
-    This specifies the application ID of the Azure application for Exchange certificate authentication.
-
-    .PARAMETER AZUREADCREDENTIAL
-
-    *REQUIRED if AzureCertificateThumbprint is not specified*
-    This is the credential utilized to connect to Azure Active Directory.
-    Global administrator is the tested permissions set / minimum permissions to execute get-azureADGroup
-
-    .PARAMETER AZUREENVRONMENTNAME
-
-    *OPTIONAL*
-    *DEFAULT:  AzureCloud*
-    This is the Azure tenant type to connect to if a non-commercial tenant is used.
-
-    .PARAMETER AZURETENANTID
-
-    *REQUIRED if AzureCertificateThumbprint is specified*
-    This is the Azure tenant ID / GUID utilized for Azure certificate authentication.
-
-    .PARAMETER AZURECERTIFICATETHUMBPRINT
-
-    *REQUIRED if AzureADCredential is not specified*
-    This is the certificate thumbprint associated with the Azure app id for Azure certificate authentication
-
-    .PARAMETER AZUREAPPLICATIONID
-
-    *REQUIRED if AzureCertificateThumbprint is specified*
-    This is the application ID assocaited with the Azure application created for certificate authentication.
-
-    .PARAMETER LOGFOLDERPATH
-
-    *REQUIRED*
-    This is the logging directory for storing the migration log and all backup XML files.
-    If running multiple SINGLE instance migrations use different logging directories.
-
-    .PARAMETER doNoSyncOU
-
-    *REQUIRED*
-    This is the organizational unit configured in Azure AD Connect to not sync.
-    This is utilize for temporary group storage to process the deletion of the group from Office 365.
-
-    .PARAMETER RETAINORIGINALGROUP
-
-    *OPTIONAL*
-    By default the original group is retained, mail disabled, and renamed with an !.
-    If the group should be deleted post migration set this value to TRUE.
-
-    .PARAMETER ENBABLEHYBRIDMAILFLOW
-
-    *OPTIONAL*
-    *REQUIRES use of ExchangeServer and ExchangeCredential*
-    This option enables mail flow objects in the on-premises Active Directory post migration.
-    This supports relay scenarios through the onpremises Exchange organization.
-
-    .PARAMETER GROUPTYPEOVERRIDE
-
-    *OPTIONAL*
-    This allows the administrator to override the group creation type in Office 365.
-    For example, an on premises security group may be migrated to Office 365 as a distribution only list.
-    If any security dependencies are discovered during the migration this option is always overridden to preserve security and the settings.
-
-    .PARAMETER TRIGGERUPGRADETOOFFICE365GROUP
-
-    *OPTIONAL*
-    *Parameter retained for backwards compatibility but now disabled.*
-
-    .PARAMETER OVERRIDECENTRALIZEDMAILTRANSPORTENABLED
-
-    *OPTIONAL*
-    If centralized transport enabled is detected during migration this switch is required.
-    This is an administrator acknowledgement that emails may flow externally in certain mail flow scenarios for migrated groups.
-
-    .PARAMETER ALLOWNONSYNCEDGROUP
-
-    *OPTIONAL*
-    Allows for on-premises group creation in Office 365 from forests that are not directory syncrhonized for some reason.
-
-    .PARAMETER USECOLLECTEDFULLMAILBOXACCESSONPREM
-
-    *OPTIONAL*
-    *Requires us of start-collectOnPremFullMailboxAccess*
-    This switch will import pre-collected full mailbox access data for the on premises organization and detect permissions for migrated DLs.
-
-    .PARAMETER USECOLLECTEDFULLMAILBOXACCESSOFFICE365
-
-    *OPTIONAL*
-    *Requires use of start-collectOffice365FullMailboxAccess
-    THis switch will import pre-collected full mailbox access data from the Office 365 organiation and detect permissions for migrated DLs.
-
-    .PARAMETER USERCOLLECTEDSENDASONPREM
-
-    *OPTIONAL*
-    *Requires use of start-collectOnPremSendAs*
-    This switch will import pre-collected send as data from the on premsies Exchange organization and detect dependencies on the migrated DLs.
-
-    .PARAMETER USECOLLECTEDFOLDERPERMISSIONSONPREM
-
-    *OPTIONAL*
-    *Requires use of start-collectOnPremMailboxFolderPermissions*
-    This switch will import pre-collected mailbox folder permissions for any default or user created folders within mailboxes.
-    The data is searched to discover any dependencies on the migrated DL.
-
-    .PARAMETER USECOLLECTEDFOLDERPERMISSIONSOFFICE365
-
-    *OPTIONAL*
-    *Requires use of start-collectOffice365MailboxFolderPermissions*
-    This switch will import pre-collected mailbox folder permissions for any default or user created folders within mailboxes.
-    The data is searched to discover any dependencies on the migrated DL.
-
-    .PARAMETER THREADNUMBERASSIGNED
-
-    *RESERVED*
-
-    .PARAMETER TOTALTHREADCOUNT
-
-    *RESERVED*
-
-    .PARAMETER ISMULTIMACHINE
-
-    *RESERVED*
-
-    .PARAMETER REMOTEDRIVELETTER
-
-    *RESERVED*
-
-    .PARAMETER ALLOWTELEMETRYCOLLECTION
-
-    Allows administrators to opt out of telemetry collection for DL migrations.  No identifiable information is collected in telemetry.
-
-    .PARAMETER ALLOWDETAILEDTELEMETRYCOLLECTIOn
-
-    Allows administrators to opt out of detailed telemetry collection.  Detailed telemetry collection includes information such as attribute member counts and time to process stages of the migration.
-
-    .PARAMETER ISHEALTHCHECK
-
-    Specifies if the function call is performing a distribution list health check.
+    This is the client secret associated with the graph application.
 
     .OUTPUTS
 
@@ -285,13 +86,30 @@ Function Start-M365DomainManagement
     (
         #Define other mandatory parameters
         [Parameter(Mandatory = $true)]
-        [string]$logFolderPath
+        [string]$logFolderPath,
+        #Define variables to allow administrator to provide grpah connectivity.
+         #Define Microsoft Graph Parameters
+        [Parameter(Mandatory = $false)]
+        [ValidateSet("","China","Global","USGov","USGovDod")]
+        [string]$msGraphEnvironmentName="",
+        [Parameter(Mandatory=$false)]
+        [string]$msGraphTenantID="",
+        [Parameter(Mandatory=$false)]
+        [string]$msGraphCertificateThumbprint="",
+        [Parameter(Mandatory=$false)]
+        [string]$msGraphApplicationID="",
+        [Parameter(Mandatory=$false)]
+        [string]$msGraphClientSecret=""
     )
 
-    #Declare local variables.
+    #Variables for logging.
 
     $global:logFile=$NULL
     $logFileName = "M365DomainMgmt_"+(Get-Date -Format FileDateTime)
+
+    #Variables for graph.
+
+    $msGraphRequiredScopes = "Domain.ReadWrite.All"
 
     #Start the log file.
 
@@ -300,5 +118,9 @@ Function Start-M365DomainManagement
     out-logfile -string "*****************************************************"
     out-logfile -string "Starting M365 Domain Management"
     out-logfile -string "*****************************************************"
+
+    out-logfile -string "Obtain and prepare MS Graph Connection."
+
+    $msGraphTenantID = get-msGraphTenantID
 }
 
