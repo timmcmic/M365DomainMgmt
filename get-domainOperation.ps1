bf1b744e0cc8b60345774be282b055bc65ff7066
Timothy J. McMichael <timmcmic@microsoft.com>
2026-03-01 17:33:21 -0500

    Updating

--- get-domainOperation.ps1
+++ get-domainOperation.ps1
@@ -14,8 +14,31 @@ function get-DomainOperation
         $domainOperations.None 
         {  
             out-logfile -string "No operation specified."
+            $domainOperation = $domainOperations.None 
+        }
+        $domainOperations.New 
+        {  
+            out-logfile -string "New operation specified."
+            $domainOperation = $domainOperations.New 
+        }
+        $domainOperations.Confirm 
+        {  
+            out-logfile -string "Confirm operation specified."
+            $domainOperation = $domainOperations.Confirm 
+        }
+        $domainOperations.Remove 
+        {  
+            out-logfile -string "Remove operation specified."
+            $domainOperation = $domainOperations.remove 
+        }
+        $domainOperations.ForceTakeOver 
+        {  
+            out-logfile -string "ForceTakeOver operation specified."
+            $domainOperation = $domainOperations.ForceTakeOver 
         }
     }
 
     out-logfile -string "Exiting get-DomainOperation"
+
+    return $domainOperation
 }
