1023ab277d97bb4e5a8380de675b273556bc2d0c
Tim McMichael <timmcmic@microsoft.com>
2026-03-10 15:04:44 -0400

    Updating

--- test-DomainName.ps1
+++ test-DomainName.ps1
@@ -6,11 +6,30 @@ function test-DomainName
         $domainName
     )
 
+    $errorString = "Request_ResourceNotFound"
+
     out-logfile -string "Entering test-DomainName"
     
     out-logfile -string "Test to see if domain is present."
 
-    $domain = get-MGDomain -domainid $domainName -errorAction STOP
+    try {
+        $domain = get-MGDomain -domainid $domainName -errorAction STOP
+
+        out-logfile -string "Successfully captured domain information."
+    }
+    catch {
+        out-logfile -string "Determine if this is a domain not found error - if so proceed."
+
+        if ($error[0].exception.toString().contains($errorString))
+        {
+            out-logfile -string "Domain is not found error."
+        }
+        else 
+        {
+            out-logfile -string $_
+            out-logfile -string "Unable to obtain domain information error." -isError:$true
+        }
+    }
 
     out-logfile -string "Exiting test-DomainName"
 
